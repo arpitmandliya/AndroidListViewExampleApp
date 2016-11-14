27e789ee54fb79c5f3476304bbd644de811c9b23
apple <apple@apples-MacBook-Air.local>
2016-11-14 18:17:39 +0530

    Initial commit

---
+++ app/src/main/java/com/java2blog/androidlistviewexampleapp/MainActivity.java
@@ -0,0 +1,37 @@
+package com.java2blog.androidlistviewexampleapp;
+
+import android.app.ListActivity;
+import android.graphics.Typeface;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.TypedValue;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends ListActivity {
+
+    String[] listofCountries={"India","China","Nepal","Bhutan"};
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setListAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, listofCountries));
+
+        TextView textView = new TextView(this);
+        textView.setTypeface(Typeface.DEFAULT_BOLD);
+        textView.setText("List of Countries");
+
+        ListView listView=(ListView)findViewById(android.R.id.list);
+        listView.addHeaderView(textView);
+    }
+
+    @Override
+    protected void onListItemClick(ListView l, View v, int position, long id) {
+        super.onListItemClick(l, v, position, id);
+        Toast.makeText(this, "You have selected : " + listofCountries[position]+ " as country", Toast.LENGTH_LONG).show();
+    }
+
+}
